8583d1a63f77990f88b098edd95993e41fab7d69
Sarahhabbi <habbisarah.1@gmail.com>
2021-12-25 22:49:01 +0100

    re implemented Server/Client/ClientHandler classes-> can chat with all clients

--- src/network/Client.java
+++ src/network/Client.java
@@ -1,128 +1,103 @@
 package network;
+
 import java.io.*;
-import java.text.*;
-import java.util.*;
-import java.net.*;
+import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
 
     private Socket socket;
-    private DataInputStream dis;
-    private DataOutputStream dos;
-    private String pseudo;
-    private Scanner scn;
+    private BufferedReader bufferedReader; //read message
+    private PrintWriter writer; //broadcast(write) messages to other clients
+    private String username;
 
+    public Client(Socket socket, String username) {
 
-    public Client(Socket socket, String pseudo) throws IOException {
-        this.socket = socket;
-        this.dis = new DataInputStream(socket.getInputStream());
-        this.dos = new DataOutputStream(socket.getOutputStream());
-        this.scn = new Scanner(System.in);
-        this.pseudo = pseudo;
-    }
-
-    public void close() {
-        // closing resources
-        try {
-            if (this.dos != null) {
-                this.dos.close();
-            }
-            if (this.dis != null) {
-                this.dis.close();
-            }
-            if(this.scn != null){
-                this.scn.close();
-            }
-            this.socket.close();
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void readMenu(){
         try{
-            //while(true){
-                String line = this.dis.readUTF();
-                System.out.println(line);
-            //}
+            this.socket = socket;
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.writer = new PrintWriter(socket.getOutputStream(), true);
+            this.username = username;
         } catch (IOException e) {
+            closeEverything(socket, bufferedReader,  writer);
             e.printStackTrace();
         }
     }
+
 
     public void sendMessage(){
         try{
-            while(true){
-                String message = this.scn.nextLine();
-                this.dos.writeUTF(this.pseudo + ": " + message);
+            writer.println(username);
+
+            Scanner scanner = new Scanner(System.in);
+
+//          send messages
+            while(socket.isConnected()){
+                String messageToSend = scanner.nextLine();
+                writer.println(username+ ": " + messageToSend);  // handled in ClientHandler
             }
-        } catch (IOException e) {
+
+        }catch(Exception e){
+            closeEverything(socket, bufferedReader, writer);
             e.printStackTrace();
         }
     }
 
-//  client will wait for message to read on dis from
-//  other clients (one thread to prevent blocking with sendMessage())
-    public void listenChannel(){
+    //  blocking operation so need a thread
+    public void listenForMessage() {
         new Thread(new Runnable() {
 
             @Override
             public void run() {
-                String messageFromChannel;
+                String messageFromChat;
 
-                while(true){
+                while(socket.isConnected()){
                     try{
-                        messageFromChannel = dis.readUTF();
-                        System.out.println(messageFromChannel);
-                    }
-                    catch (Exception e) {
-                        close();
-                        e.printStackTrace();
+                        messageFromChat = bufferedReader.readLine(); // read message broadcasted by CientHandler
+                        System.out.println(messageFromChat);
+                    } catch (IOException e) {
+                        closeEverything(socket, bufferedReader, writer);
+                        break;
                     }
                 }
             }
         }).start();
     }
 
-    public static void main(String[] args) throws IOException
-    {
+    public void closeEverything(Socket socket, BufferedReader bufferedReader, PrintWriter writer) {
+
+        try{
+            if(bufferedReader != null){
+                bufferedReader.close();
+            }
+            if(writer != null){
+                writer.close();
+            }
+            if(socket != null){
+                socket.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
         try
         {
-            // establish the connection with server port 5056
-            Socket s = new Socket("localhost", 1234);
+            // ask username
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter your username:");
+            String username = scanner.nextLine();
 
-        /*    Scanner scanner = new Scanner(System.in);
-            System.out.println("Enter your pseudo:");
-            scanner.nextLine();
-            String pseudo = scanner.nextLine();*/
+            // establish the connection with server port 1234
+            Socket socket = new Socket("localhost", 1234);
 
-            //create client
-            Client client = new Client(s, "sarah");
+            // create Client
+            Client client = new Client(socket, username);
 
-            // the following loop performs the exchange of
-            // information between client and client handler
-            client.readMenu();
+            client.listenForMessage();
             client.sendMessage();
-            client.listenChannel();
 
-//            while (true)
-//            {
-//                System.out.println(client.dis.readUTF());
-//                String tosend = client.scn.nextLine();
-//                client.dos.writeUTF(tosend);
-//
-//                // If client sends exit,close this connection
-//                // and then break from the while loop
-//                if(tosend.equals("Quit"))
-//                {
-//                    System.out.println("Closing this connection : " + s);
-//                    s.close();
-//                    System.out.println("Connection closed");
-//                    break;
-//                }
-//
-//            }
         }catch(Exception e){
             e.printStackTrace();
         }
---
+++ src/network/ClientHandler.java
@@ -0,0 +1,86 @@
+package network;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ClientHandler implements Runnable {
+
+    private static ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
+    private Socket socket;
+    private BufferedReader bufferedReader; //read message
+    private PrintWriter writer; //broadcast(write) messages to other clients
+    private String clientUsername;
+
+    public ClientHandler(Socket socket) {
+        try{
+            this.socket = socket;
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.writer = new PrintWriter(socket.getOutputStream(), true);
+
+            this.clientUsername = bufferedReader.readLine();  // username is sent in sendMessage() method in Client class
+            this.clientHandlers.add(this);
+            this.broadcastMessage("SERVER: " + clientUsername + " has entered the chat !");
+        } catch (IOException e) {
+            closeEverything(socket, bufferedReader, writer);
+            e.printStackTrace();
+        }
+    }
+
+//    everything in this method will be run in a separate thread, we will listen to new messages (blocking operation)
+    @Override
+    public void run() {
+        String messageFromClient;
+
+        while(socket.isConnected()){
+            try{
+                messageFromClient = bufferedReader.readLine(); // blocking operation but here it's in another thread so no problem
+                broadcastMessage(messageFromClient);
+
+            } catch (IOException e) {
+                closeEverything(socket, bufferedReader, writer);
+                break;
+            }
+        }
+    }
+
+    public void broadcastMessage(String messageToSend) {
+        System.out.println(">>> broadcastMessage() message =  "+ messageToSend);
+
+        /* iterate over each client and write in every output stream the message except to the client who sent */
+        for(ClientHandler clientHandler : clientHandlers){
+            try{
+                if(!clientHandler.clientUsername.equals(this.clientUsername)){
+                    clientHandler.writer.println(messageToSend);
+                }
+            } catch (Exception e) {
+                closeEverything(socket, bufferedReader, writer);
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public void removeClientHandler(){
+        clientHandlers.remove(this);
+        broadcastMessage("SERVER: "+ clientUsername+ " has left the chat");
+    }
+
+    public void closeEverything(Socket socket, BufferedReader bufferedReader, PrintWriter writer) {
+        removeClientHandler();
+        try{
+            if(bufferedReader != null){
+                bufferedReader.close();
+            }
+            if(writer != null){
+                writer.close();
+            }
+            if(socket != null){
+                socket.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
