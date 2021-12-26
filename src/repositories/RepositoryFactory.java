b41edfbc1487eda0138e7c750cf3d80f2e8cc767
Sarahhabbi <habbisarah.1@gmail.com>
2021-12-26 15:11:09 +0100

    conflict with merges resolved

--- src/repositories/RepositoryFactory.java
+++ src/repositories/RepositoryFactory.java
@@ -9,13 +9,10 @@ public class RepositoryFactory {
         return new CompositeRepository<>(new UserRepository());
     }
 
-<<<<<<< HEAD
     public static Repository<Channel> channel(){
         return new CompositeRepository<>(new ChannelRepository());
     }
-=======
-    public static Repository<Channel> channel(String filename) { return new CompositeRepository<>(new ChannelRepository(filename)); }
->>>>>>> main
+//    public static Repository<Channel> channel(String filename) { return new CompositeRepository<>(new ChannelRepository(filename)); }
 
     public static Repository<Channel> channel_user(String filename){
         return new CompositeRepository<>(new ChannelUserRepository(filename));
